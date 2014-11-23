2aa6974fcce916ed8495bfdf8baf989c8c25b9e3
Eddy Davidov <EddyD@users.noreply.github.com>
2014-11-23 19:12:49 +0200

    Create ball.js

---
+++ ball.js
@@ -0,0 +1,9 @@
+function startJump(){
+	document.getElementById("ball").style.webkitAnimation="ball 10s"
+	
+
+}
+
+function stopJump(){
+	document.getElementById("ball").style.webkitAnimation="";
+}
